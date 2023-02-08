168c4ec16d7c83357798c0f64f53da410d1e0010
Lawrence-tech <lawrencesagini2@gmail.com>
2023-02-08 04:23:50 -0800

    pchar

---
+++ stack_pchar.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+  *stack_pchar - print the top ASCII character of linked list.
+  *@head: pointer to the head of the stack.
+  *@l_num: the number line of command.
+  */
+void stack_pchar(stack_t **head, unsigned int l_num)
+{
+	if (head == NULL || *head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", l_num);
+		free_vars();
+		exit(EXIT_FAILURE);
+	}
+	if ((*head)->n < 0 || (*head)-> >= 128)
+	{
+		fprintf(stderr, "L%u: can't pchar, value out of ranger\n", l_num);
+		free_vars();
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", (*head)->n);
+}
