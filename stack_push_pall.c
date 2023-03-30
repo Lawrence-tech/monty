67ddd3d8cbeb2ae35a407055d8e6ac8d846c79bf
Lawrence.tech <lawrencesagini2@gmail.com>
2023-03-29 22:17:16 -0700

    modified to fix error

--- stack_push_pall.c
+++ stack_push_pall.c
@@ -3,45 +3,60 @@
 #include <unistd.h>
 #include <stdio.h>
 #include "monty.h"
+
 /**
-  *stack_push - pushes an element to the stack.
-  *@head: pointer to the head of the stack.
-  *@l_num: line number of the command
-  */
+ * stack_push - pushes an element to the stack.
+ * @head: pointer to the head of the stack.
+ * @l_num: line number of the command
+ */
 void stack_push(stack_t **head, unsigned int l_num)
 {
-	int number;
-	stack_t *temp = vars.stack;
-	char str[100];
+	stack_t *new_node;
 
-	if (!temp)
+	if (!vars.stack)
 	{
-		fprintf(stderr, "L%u: ", l_num);
-		fprintf(stderr, "usage: push integer\n");
+		fprintf(stderr, "L%u: usage: push integer\n", l_num);
 		free_vars();
 		exit(EXIT_FAILURE);
 	}
 
-	sprintf(str, "%d", temp->n);
-	if (!isdigit(*str) && *str != '-')
+	errno = 0;
+	long int value = strtol(vars.stack->n, NULL, 10);
+	if (errno == ERANGE || value < INT_MIN || value > INT_MAX)
 	{
-		fprintf(stderr, "L%d: usage: push integer\n", l_num);
+		fprintf(stderr, "L%u: usage: push integer\n", l_num);
 		free_vars();
 		exit(EXIT_FAILURE);
 	}
 
-	number = atoi(str);
-	if (vars.order == 1)
-		add_dnodeint(head, number);
+	new_node = malloc(sizeof(stack_t));
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free_vars();
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = (int)value;
+	new_node->prev = NULL;
+
+	if (*head)
+	{
+		new_node->next = *head;
+		(*head)->prev = new_node;
+	}
 	else
-	add_dnodeint_end(head, number);
+		new_node->next = NULL;
+
+	*head = new_node;
+	vars.stack = vars.stack->next;
 }
 
 /**
-  *stack_pall - prints all the values on the stack, starting from the top.
-  *@head: pointer to the head of the stack.
-  *@l_num: line number of the command.
-  */
+ * stack_pall - prints all the values on the stack, starting from the top.
+ * @head: pointer to the head of the stack.
+ * @l_num: line number of the command.
+ */
 void stack_pall(stack_t **head, unsigned int l_num)
 {
 	stack_t *current = *head;
